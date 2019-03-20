407b7e7e1907e39e258b4e2b8289859e36c046de
Daedes <lbelliott3995@gmail.com>
2019-03-20 00:28:18 -0400

    Add files via upload

---
+++ JavaEEcode/src/softprojectpackage/EvaluationServlet.java
@@ -0,0 +1,43 @@
+package softprojectpackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EvaluationServlet
+ */
+@WebServlet("/EvaluationServlet")
+public class EvaluationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	int quality;
+	int dependability;
+	int communication;
+    public EvaluationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");  
+        PrintWriter out=response.getWriter();  
+        request.getRequestDispatcher("link.html").include(request, response);  
+          
+         this.quality= Integer.parseInt(request.getParameter("quality"));  
+         out.print(quality);
+         
+         this.dependability=  Integer.parseInt(request.getParameter("dependability"));
+         
+         this.communication =  Integer.parseInt(request.getParameter("communication"));
+	}
+
+}
