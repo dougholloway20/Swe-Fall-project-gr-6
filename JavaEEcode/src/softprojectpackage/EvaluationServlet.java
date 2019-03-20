bb23c0e63037f0c70bd256331f79744ebe4d340d
Daedes <lbelliott3995@gmail.com>
2019-03-20 13:53:45 -0400

    Fixed server crashing
    Fixed the server not starting due to this class. Everything should work.

--- JavaEEcode/src/softprojectpackage/EvaluationServlet.java
+++ JavaEEcode/src/softprojectpackage/EvaluationServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class EvaluationServlet
+ * Servlet implementation class EvaluationServlet fixed
  */
 
 public class EvaluationServlet extends HttpServlet {
