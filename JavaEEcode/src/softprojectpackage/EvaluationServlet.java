9f0f4cdb5204283ee8f7b6ae7612b8f59726783a
Daedes <lbelliott3995@gmail.com>
2019-03-20 13:52:22 -0400

    Update EvaluationServlet.java

--- JavaEEcode/src/softprojectpackage/EvaluationServlet.java
+++ JavaEEcode/src/softprojectpackage/EvaluationServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EvaluationServlet
  */
-@WebServlet("/EvaluationServlet")
+
 public class EvaluationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
